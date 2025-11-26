9552da8f27c4e3809837002a1e5cf572d6054eec
kjm <kjmesu@proton.me>
2025-11-26 17:51:26 +0000

    feat: Load first post vote data directly in topic list serializer
    This removes the request queue and loads first post vote data directly into the topic list item serializer. This avoids rate limits and improves rendering performance.

--- plugin.rb
+++ plugin.rb
@@ -3,24 +3,48 @@
 after_initialize do
   next unless defined?(PostVotesVote)
 
+  module ::TopicListItemPostVotesSerializerExtensions
+    def post_votes_first_post
+      return @post_votes_first_post if defined?(@post_votes_first_post)
+
+      post =
+        if object.association(:first_post).loaded?
+          object.first_post
+        elsif object.association(:posts).loaded?
+          object.posts.find { |p| p.post_number == 1 }
+        end
+
+      post ||=
+        Post
+          .where(topic_id: object.id, post_number: 1)
+          .select(:id, :topic_id, :post_votes_score)
+          .first
+
+      @post_votes_first_post = post
+    end
+  end
+
+  ::TopicListItemSerializer.include TopicListItemPostVotesSerializerExtensions
+
   add_to_serializer(:topic_list_item, :first_post_post_votes_count) do
     next unless SiteSetting.post_votes_enabled
-    object.first_post&.post_votes_score.to_i
+    post_votes_first_post&.post_votes_score.to_i
   end
 
   add_to_serializer(:topic_list_item, :first_post_post_votes_has_votes) do
     next unless SiteSetting.post_votes_enabled
-    (object.first_post&.post_votes_score.to_i || 0) > 0
+    (post_votes_first_post&.post_votes_score.to_i || 0) > 0
   end
 
   add_to_serializer(:topic_list_item, :first_post_post_votes_user_direction) do
     next unless SiteSetting.post_votes_enabled
     next unless scope&.user
-    next unless object.first_post
+    post = post_votes_first_post
+    next unless post
 
     PostVotesVote.find_by(
       votable_type: "Post",
-      votable_id: object.first_post.id,
+      votable_id: post.id,
       user_id: scope.user.id
     )&.direction
   end
